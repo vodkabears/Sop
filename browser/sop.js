0ffd0bd0473e05edde58218159c2464d4eb17874
VodkaBears <dfrost.00@gmail.com>
2015-08-01 00:30:11 +0300

    Rename Ovi to Sop

--- browser/sop.js
+++ browser/sop.js
@@ -1,12 +1,12 @@
 /*
- *  Ovi - v0.0.1
- *  Micro-library for parsing stringified parameters or converting parameters to the string representation.
- *  http://vodkabears.github.io/ovi/
+ *  Sop - v0.0.1
+ *  Small library for parsing stringified properties or converting object properties to the string representation.
+ *  http://vodkabears.github.io/sop/
  *
  *  Made by Ilya Makarov
  *  Under MIT License
  */
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Ovi = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.Sop = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
 'use strict';
 var extend = require('extend');
 
--- test/parse.js
+++ test/parse.js
@@ -1,68 +1,68 @@
-var Ovi = require('../');
+var sop = require('../');
 var assert = require('proclaim');
 
 describe('.parse(str, opts)', function() {
   it('should correctly parse `null`', function() {
-    assert.deepEqual(Ovi.parse('value: null'), { value: null });
+    assert.deepEqual(sop.parse('value: null'), { value: null });
   });
 
   it('should correctly parse `undefined`', function() {
-    assert.deepEqual(Ovi.parse('value: undefined'), { value: undefined });
+    assert.deepEqual(sop.parse('value: undefined'), { value: undefined });
   });
 
   describe('for boolean values', function() {
     it('should correctly parse `true`', function() {
-      assert.deepEqual(Ovi.parse('value: true'), { value: true });
+      assert.deepEqual(sop.parse('value: true'), { value: true });
     });
 
     it('should correctly parse `false`', function() {
-      assert.deepEqual(Ovi.parse('value: false'), { value: false });
+      assert.deepEqual(sop.parse('value: false'), { value: false });
     });
   });
 
   describe('for numeric values', function() {
     it('should correctly parse a simple integer number', function() {
-      assert.deepEqual(Ovi.parse('value: 1000'), { value: 1000 });
+      assert.deepEqual(sop.parse('value: 1000'), { value: 1000 });
     });
 
     it('should correctly parse a number with "0x" or "0X" in the beginning', function() {
-      assert.deepEqual(Ovi.parse('val1: 0x1000, val2: 0X1000'), { val1: 4096, val2: 4096 });
+      assert.deepEqual(sop.parse('val1: 0x1000, val2: 0X1000'), { val1: 4096, val2: 4096 });
     });
 
     it('should correctly parse a float', function() {
-      assert.deepEqual(Ovi.parse('value: 1000.1'), { value: 1000.1 });
+      assert.deepEqual(sop.parse('value: 1000.1'), { value: 1000.1 });
     });
   });
 
   describe('for string values', function() {
     it('should correctly parse a string without quotes', function() {
-      assert.deepEqual(Ovi.parse('value: http://localhost:8080'), { value: 'http://localhost:8080' });
+      assert.deepEqual(sop.parse('value: http://localhost:8080'), { value: 'http://localhost:8080' });
     });
 
     it('should correctly parse a string with quotes', function() {
-      assert.deepEqual(Ovi.parse('value: "http://localhost:8080"'), { value: 'http://localhost:8080' });
+      assert.deepEqual(sop.parse('value: "http://localhost:8080"'), { value: 'http://localhost:8080' });
     });
 
     it('should correctly parse `null` with quotes', function() {
-      assert.deepEqual(Ovi.parse('value: "null"'), { value: 'null' });
+      assert.deepEqual(sop.parse('value: "null"'), { value: 'null' });
     });
 
     it('should correctly parse `undefined` with quotes', function() {
-      assert.deepEqual(Ovi.parse('value: "undefined"'), { value: 'undefined' });
+      assert.deepEqual(sop.parse('value: "undefined"'), { value: 'undefined' });
     });
 
     it('should correctly parse a boolean with quotes', function() {
-      assert.deepEqual(Ovi.parse('value: "true"'), { value: 'true' });
+      assert.deepEqual(sop.parse('value: "true"'), { value: 'true' });
     });
 
     it('should correctly parse a number with quotes', function() {
-      assert.deepEqual(Ovi.parse('value: "1000"'), { value: '1000' });
+      assert.deepEqual(sop.parse('value: "1000"'), { value: '1000' });
     });
   });
 
   describe('for mixed', function() {
     it('should correctly parse mixed', function() {
-      assert.deepEqual(Ovi.parse('num: 10, bool: false, str: http://te.st:80'), {
+      assert.deepEqual(sop.parse('num: 10, bool: false, str: http://te.st:80'), {
         num: 10,
         bool: false,
         str: 'http://te.st:80'
@@ -70,7 +70,7 @@ describe('.parse(str, opts)', function() {
     });
 
     it('should correctly parse a bad formated string', function() {
-      assert.deepEqual(Ovi.parse('num: 10,bool: false, str:http://te.st:80'), {
+      assert.deepEqual(sop.parse('num: 10,bool: false, str:http://te.st:80'), {
         num: 10,
         bool: false,
         str: 'http://te.st:80'
@@ -78,7 +78,7 @@ describe('.parse(str, opts)', function() {
     });
 
     it('should correctly parse a string with invalid properties', function() {
-      assert.deepEqual(Ovi.parse('num: 10, boolfalse, str:http://te.st:80'), {
+      assert.deepEqual(sop.parse('num: 10, boolfalse, str:http://te.st:80'), {
         num: 10,
         str: 'http://te.st:80'
       });
@@ -86,7 +86,7 @@ describe('.parse(str, opts)', function() {
   });
 
   it('should correctly parse when the key-value delimiter is `=` and the delimiter of properies is `&`', function() {
-    assert.deepEqual(Ovi.parse('num=10&bool=false', {
+    assert.deepEqual(sop.parse('num=10&bool=false', {
       keyValDelim: '=',
       propsDelim: '&'
     }), { num: 10, bool: false });
